29bc3fcdbcb03bf9a10d75a0e76a68d089c34eab
Employee427 <minecraftmason01@gmail.com>
2025-05-22 02:51:12 -0700

    Add files via upload

--- src/main/kotlin/me/shadymitsu/cobblemonshinydays/config/ConfigLoader.kt
+++ src/main/kotlin/me/shadymitsu/cobblemonshinydays/config/ConfigLoader.kt
@@ -5,6 +5,7 @@ import com.google.gson.reflect.TypeToken
 import java.io.File
 import java.io.FileReader
 
+
 object ConfigLoader {
 
     private val configFile = File("config/cobblemonshinydays/config.json")
